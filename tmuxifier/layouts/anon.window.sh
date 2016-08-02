85edf52669dab8b5f1752b723f32021443a6aeee
TheoKlein <theokleintw@gmail.com>
2016-08-02 13:32:18 +0800

    update layouts

--- tmuxifier/layouts/anon.window.sh
+++ tmuxifier/layouts/anon.window.sh
@@ -9,14 +9,20 @@ new_window "anon"
 # Split window into panes.
 #split_v 20
 split_h 20
+split_v 20
 
 # Run commands.
 run_cmd "vim" 0    # runs in active pane
 run_cmd "git status" 1  # runs in pane 1
+run_cmd "glances --disable-load" 2
 
 # Paste text
 #send_keys "top"    # paste into active pane
 #send_keys "date" 1 # paste into pane 1
+send_keys "4" 2
+send_keys "l" 2
+send_keys "2" 2
+send_keys "z" 2
 
 # Set active pane.
 select_pane 0
--- tmuxifier/layouts/zerojudge.window.sh
+++ tmuxifier/layouts/zerojudge.window.sh
@@ -7,15 +7,22 @@ window_root "~/GoogleDrive/ProgrammingCode/ZeroJudge/"
 new_window "zerojudge"
 
 # Split window into panes.
-split_h 15
+split_h 20
+split_v 20
 
 # Run commands.
 #run_cmd "top"     # runs in active pane
 #run_cmd "date" 1  # runs in pane 1
+run_cmd "vim" 0
+run_cmd "glances --disable-load" 2
 
 # Paste text
 #send_keys "top"    # paste into active pane
 #send_keys "date" 1 # paste into pane 1
+send_keys "4" 2
+send_keys "l" 2
+send_keys "2" 2
+send_keys "z" 2
 
 # Set active pane.
 select_pane 0
