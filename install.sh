2b6724dd8999b67fff6348077b5ade1d0b7c0c89
Theo Klein <theokleintw@gmail.com>
2017-08-31 16:10:39 +0800

    update

--- install.sh
+++ install.sh
@@ -6,16 +6,12 @@ cp tools/tmux/.tmux.conf ~/
 # Install tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-source ~/.tmux.conf
-
 # Copy .vimrc
 cp tools/vim/.vimrc ~/
 
 # Copy vim
 cp -r tools/vim ~/.vim
 rm ~/.vim/.vimrc
-
-source ~/.vimrc
 
 # ubuntu .bashrc
 cp ubuntu/.bashrc ~/
